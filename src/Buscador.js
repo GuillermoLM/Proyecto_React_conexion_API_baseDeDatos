bedfd97dcc062e9b250fe3c465270ff8e75c577c
Guillermo <you@example.com>
2018-08-21 17:03:32 +0200

    Google Places API done and input in Buscador works

--- src/Buscador.js
+++ src/Buscador.js
@@ -1,22 +1,139 @@
 import React, { Component } from 'react';
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+  } from 'react-places-autocomplete';
+import {classnames} from "./helpers";
 import './Buscador.css';
 
 export default class Buscador extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          address: '',
+          errorMessage: '',
+          latitude: null,
+          longitude: null,
+          isGeocoding: false,
+        };
+      }
+    
+    handleChange = address => {
+        this.setState({
+          address,
+          latitude: null,
+          longitude: null,
+          errorMessage: '',
+        });
+    };
+    
+    handleSelect = selected => {
+        this.setState({ isGeocoding: true, address: selected });
+        geocodeByAddress(selected)
+          .then(res => getLatLng(res[0]))
+          .then(({ lat, lng }) => {
+            this.setState({
+              latitude: lat,
+              longitude: lng,
+              isGeocoding: false,
+            });
+            })
+          .catch(error => {
+            this.setState({ isGeocoding: false });
+            console.log('error', error); // eslint-disable-line no-console
+          });
+      };
+    
+    handleCloseClick = () => {
+        this.setState({
+          address: '',
+          latitude: null,
+          longitude: null,
+        });
+    };
+    
+    handleError = (status, clearSuggestions) => {
+        console.log('Error from Google Maps API', status); // eslint-disable-line no-console
+        this.setState({ errorMessage: status }, () => {
+          clearSuggestions();
+        });
+    };
+
     render(){
+        const {
+            address,
+            errorMessage,
+            latitude,
+            longitude,
+            isGeocoding,
+          } = this.state;
         return(
             <div className="Buscador">
-                <form action="https://dev.entrenar.me/api/v3/oauth/access_token" method="post"onSubmit={this.handleSubmit}>
+                <form>
                     <div className="row">
                         <div className="col-6 d-flex justify-content-end">
-                            <FormGroup controlId="ciudad" bsSize="small">
-                            <ControlLabel>Ciudad</ControlLabel>
-                            <FormControl
-                                autoFocus
-                                type="text"
-                            />
-                             </FormGroup>
+                            <div>
+                                <PlacesAutocomplete
+                                    onChange={this.handleChange}
+                                    value={address}
+                                    onSelect={this.handleSelect}
+                                    onError={this.handleError}
+                                    shouldFetchSuggestions={address.length > 2}
+                                >
+                                {({ getInputProps, suggestions, getSuggestionItemProps }) => {
+                                return (
+                                    <div className="search-bar-container">
+                                        <div className="search-input-container">
+                                            <input
+                                                {...getInputProps({
+                                                placeholder: 'Busca tu ciudad...',
+                                                className: 'search-input',
+                                                })}
+                                            />
+                                            {this.state.address.length > 0 && (
+                                            <button
+                                                className="clear-button"
+                                                onClick={this.handleCloseClick}
+                                            >
+                                            x
+                                            </button>
+                                            )}
+                                        </div>
+                                        {suggestions.length > 0 && (
+                                        <div className="autocomplete-container">
+                                            {suggestions.map(suggestion => {
+                                                const className = classnames('suggestion-item', {
+                                                    'suggestion-item--active': suggestion.active,
+                                                });
+                                                return (
+                                                /* eslint-disable react/jsx-key */
+                                                    <div
+                                                        {...getSuggestionItemProps(suggestion, { className })}
+                                                    >
+                                                    <strong>
+                                                        {suggestion.formattedSuggestion.mainText}
+                                                    </strong>{' '}
+                                                    <small>
+                                                        {suggestion.formattedSuggestion.secondaryText}
+                                                    </small>
+                                                </div>
+                                                );
+                                                /* eslint-enable react/jsx-key */
+                                            })}
+                                        </div>
+                                    )}
+                                    </div>
+                                );
+                                }}
+                                </PlacesAutocomplete>
+                                    {errorMessage.length > 0 && (
+                                        <div className="error-message">{this.state.errorMessage}</div>
+                                    )}
+                            </div>
                         </div>
+                        
                         <div className="col-6 d-flex justify-content-start">
                             <FormGroup controlId="deporte" bsSize="small">
                             <ControlLabel>Deporte</ControlLabel>
@@ -39,4 +156,6 @@ export default class Buscador extends Component {
         );
     }
 }
+
+
 
